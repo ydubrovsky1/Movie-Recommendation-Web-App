e2e97a7f3b093f3d209703b6ebd8f634cf7ec990
Kyle Jones <kylemjones.b@gmail.com>
2021-12-08 09:23:49 -0500

    added password to db create file, changed .env

--- java/database/create.sh
+++ java/database/create.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 BASEDIR=$(dirname $0)
 DATABASE=final_capstone
+export PGPASSWORD='postgres1'
 psql -U postgres -f "$BASEDIR/dropdb.sql" &&
 createdb -U postgres $DATABASE &&
 psql -U postgres -d $DATABASE -f "$BASEDIR/schema.sql" &&
