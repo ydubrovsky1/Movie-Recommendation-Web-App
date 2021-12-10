59f19d1feecbd6f7a8e37151c5710c964b41fda8
Kyle Jones <kylemjones.b@gmail.com>
2021-12-10 16:46:16 -0500

    updated schema and jdbcs

--- java/src/main/java/com/techelevator/controller/MovieController.java
+++ java/src/main/java/com/techelevator/controller/MovieController.java
@@ -29,6 +29,6 @@ public class MovieController {
         return genreDao.save(user.getId(), user.getPreferredGenres());
     }
 
-    @RequestMapping(path = "/")
+   // @RequestMapping(path = "/")
 
 }
--- java/src/main/java/com/techelevator/dao/JdbcMovieDao.java
+++ java/src/main/java/com/techelevator/dao/JdbcMovieDao.java
@@ -1,4 +1,40 @@
 package com.techelevator.dao;
 
+import com.techelevator.model.Movie;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+
+import java.sql.Array;
+
 public class JdbcMovieDao {
+
+    private JdbcTemplate jdbcTemplate;
+
+    public JdbcMovieDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public Movie findMovieById(int movieId) {
+        String sql = "SELECT movie_id, title, " +
+                "overview, runtime, director, actors, release_date, rating, " +
+                "certification, genres FROM movies " +
+                "WHERE movie_id = ?;";
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql, movieId);
+
+        if (results.next()) {
+
+        }
+        return null;
+    }
+        //SELECT * from movies where 20 = ANY(genres);
+    public Movie mapRowToMovie(SqlRowSet rs) {
+        Movie movie = new Movie();
+        movie.setMovieId(rs.getInt("movie_id"));
+        movie.setTitle(rs.getString("title"));
+        movie.setOverview(rs.getString("overview"));
+        movie.setRuntime(rs.getInt("runtime"));
+        movie.setDirector(rs.getString("director"));
+        //Array g = rs.getArray
+        return movie;
+    }
 }
